49769041bd56c1c50662391e7c99bff329701384
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-23 12:59:22 -0700

    built navbar in form builder.

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -1,7 +1,14 @@
+import { GetFormById } from "@/actions/form";
+import FormBuilder from "@/components/FormBuilder";
 import React from "react";
 
-function BuilderPage({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+async function BuilderPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const form = await GetFormById(Number(id));
+  if (!form) {
+    throw new Error("form not found");
+  }
+  return <FormBuilder form={form} />;
 }
 
 export default BuilderPage;
