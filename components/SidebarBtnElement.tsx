7417252b5b928a43949e9241144b4e05aec850dc
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-25 11:36:08 -0700

    built publish, save and visit buttons.

--- components/SidebarBtnElement.tsx
+++ components/SidebarBtnElement.tsx
@@ -13,6 +13,7 @@ function SidebarBtnElement({ formElement }: { formElement: FormElement }) {
       isDesignerBtnElement: true,
     },
   });
+
   return (
     <Button
       ref={draggable.setNodeRef}
@@ -32,13 +33,7 @@ function SidebarBtnElement({ formElement }: { formElement: FormElement }) {
 
 export function SidebarBtnElementDragOverlay({ formElement }: { formElement: FormElement }) {
   const { label, icon: Icon } = formElement.designerBtnElement;
-  const draggable = useDraggable({
-    id: `designer-btn-${formElement.type}`,
-    data: {
-      type: formElement.type,
-      isDesignerBtnElement: true,
-    },
-  });
+
   return (
     <Button variant={"outline"} className="flex flex-col gap-2 h-[120px] w-[120px] cursor-grab">
       <Icon className="h-8 w-8 text-primary cursor-grab" />
